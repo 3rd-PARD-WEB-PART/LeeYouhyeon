fc31d4be9ddd8ad9100d12f9b7be3402219e572d
LYH513 <peperony367@gmail.com>
2024-03-17 01:12:59 +0900

    걷기 애니메이션 css 추가

--- pard_lyh_introduce/src/App.js
+++ pard_lyh_introduce/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import walk from "./Assets/walk1.gif";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="container-box">
+          <img src={walk} className="walk_image"></img>
         </div>
       </div>
       <div className="App_orangeline1"></div>
