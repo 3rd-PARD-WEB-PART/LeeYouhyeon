48e8ab595d1e6f0c45b3616560c4d168093c8c8c
LYH513 <peperony367@gmail.com>
2024-03-17 23:28:49 +0900

    자기소개 내용 수정

--- pard_lyh_introduce/src/App.js
+++ pard_lyh_introduce/src/App.js
@@ -16,8 +16,8 @@ function App() {
             <span className="talk1">안녕하세요! </span>
             <span className="talk2">저는 21학번 언론정보/ICT 융합 이유현입니다! </span>
             <span className="talk3">좋아하는 것은 바닐라 라떼, 포켓몬 게임, 고양이입니다. </span>
-            <span className="talk4">많이 부족하지만 OB로서 열심히 도와드리겠습니다. </span>
-            <span className="talk5">잘 부탁드려요!</span>
+            <span className="talk4">이번 학기 목표는 배운 내용 꾸준히 복습하기입니다.</span>
+            <span className="talk5">다들 잘 부탁드려요!</span>
           </div>
           <img src={pokemon} className="pokemon_img"></img>
           <img src={cat} className="cat_img"></img>
