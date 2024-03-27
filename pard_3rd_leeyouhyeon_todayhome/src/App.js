da25e784163bd802ffd806fe9265eba79071c973
LYH513 <peperony367@gmail.com>
2024-03-27 23:25:16 +0900

    과제1 폴더 및 파일 생성

---
+++ pard_3rd_leeyouhyeon_todayhome/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RegisterPage from "./Pages/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterPage/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ pard_3rd_leeyouhyeon_todayhome/src/Pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function RegisterPage() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+
+export default RegisterPage;
