0dc68386fc49e9606d04cb27962cac149fd2c40b
LYH513 <peperony367@gmail.com>
2024-04-04 20:32:20 +0900

    헤더 컴포넌트 추가

---
+++ pard_3rd_leeyouhyeon_todayhome/src/Header.js
@@ -0,0 +1,214 @@
+import React, {useRef,useState} from "react";
+import styled from "styled-components";
+import Header_1 from "./Assets/Header_1.png"
+import seach_icon from "./Assets/검색 아이콘.png"
+import scrap from "./Assets/스크랩 아이콘.png"
+import alarm from "./Assets/알림 아이콘.png"
+import basket from "./Assets/장바구니 아이콘.png"
+import image from "./Assets/stand.png"
+import vector from "./Assets/Vector 4.png"
+
+function Header(){
+
+    return(
+        <Container>
+            <HeaderMain>
+                <img src={Header_1} alt="오늘의 집 아이콘"></img>
+            </HeaderMain>
+            <CategoryDiv>
+                <HeaderOption>커뮤니티</HeaderOption>
+                <HeaderOption>쇼핑</HeaderOption>
+                <HeaderOption>인테리어/생활</HeaderOption>
+            </CategoryDiv>
+            <SearchDiv >
+                <img src={seach_icon} alt="검색 아이콘"></img>
+                <input type="text" placeholder="통합검색" ></input>
+            </SearchDiv>
+            <SmallIconDiv>
+                <button style={{marginRight: "21px"}}>
+                    <img src={scrap} alt="스크랩 아이콘" ></img>
+                </button>
+                <button style={{marginRight: "18px"}}>
+                    <img src={alarm} alt="알림 아이콘" ></img>
+                </button>
+                <button>
+                    <img src={basket} alt="장바구니 아이콘"></img>
+                </button>
+    
+            </SmallIconDiv>
+            <ProfileImgDiv>
+                <button>
+                    <div></div> 
+                </button>
+            </ProfileImgDiv> 
+            <WrightingBtn>
+                <div>글쓰기</div>
+                <img src={vector}></img>
+            </WrightingBtn>
+
+        </Container>
+    );
+}
+
+const Container =styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 86px;
+    border-bottom: 1px solid #EAEBEF;
+    background: #FFF;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+
+`;
+
+const HeaderMain =styled.button`
+    display: flex;
+    border: none;
+    background-color: white;
+    width: 85px;
+    height: 41.488px;
+    flex-shrink: 0;
+    margin-right: 50px;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    > img {
+        display: flex;
+    }
+    
+`;
+
+const CategoryDiv =styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    margin-right: 118px;
+`;
+
+const HeaderOption =styled.button`
+    color: #000;
+    text-align: center;
+    font-family: Inter;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    
+    border: none;
+    background-color: white;
+    cursor: pointer;
+`
+
+const SearchDiv =styled.button`
+    display: flex;
+    flex-direction: row;
+    width: 360px;
+    height: 43px;
+    flex-shrink: 0;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+
+    border-radius: 3px;
+    border: 1px solid #DADDE0;
+    margin-right: 29px;;
+
+    > img {
+        display: flex;
+        width: 19.835px;
+        height: 20.5px;
+        flex-shrink: 0;
+    }
+
+    > input {
+        display: flex;
+
+        text-align: start;
+        font-family: Inter;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        width: 290px;
+        padding-left: 10px;
+        border: none;
+        outline: none;
+
+        &::placeholder{ //인풋의 플레이스홀더의 폰트 css에 접근
+            color: #DBDBDB;
+        }
+
+    }
+`;
+
+const SmallIconDiv =styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-right: 23px;
+
+    >button{
+        display: flex;
+        background-color: white;
+        border: none;
+        padding: 0;
+        cursor: pointer;
+    }
+
+`;
+
+const ProfileImgDiv =styled.div`
+    display: flex;
+    /* border: none; */
+    width: 43px;
+    height: 43px;
+    flex-shrink: 0;
+    margin-right: 40px;
+
+    > button {
+        display: flex;
+        width: 43px;
+        height: 43px;
+        border: none;
+        padding: 0;
+        border-radius: 50%;
+        background-color: #DBDBDB;
+        background-image: url(${image});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: 40px;
+        cursor: pointer;
+    }
+    
+
+`
+
+const WrightingBtn =styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 43px;
+    flex-shrink: 0;
+    border-radius: 5px;
+    background: #35C5F0;
+    border: none;
+
+    color: #FFF;
+    text-align: center;
+    font-family: Inter;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    gap: 8px;
+    cursor: pointer;
+`;
+
+export default Header;
--- pard_3rd_leeyouhyeon_todayhome/src/Pages/ProfilePage/Profile.js
+++ pard_3rd_leeyouhyeon_todayhome/src/Pages/ProfilePage/Profile.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "../../Header";
 
 function Profile() {
   return (
     <div>
+      <Header/>
     </div>
   );
 }
