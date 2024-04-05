c85a666b440b3b60478ec89c4f9d88dc2cfe61b9
LYH513 <peperony367@gmail.com>
2024-04-05 16:00:21 +0900

    프로필 편집 페이지 헤더 컴포넌트 추가

--- pard_3rd_leeyouhyeon_todayhome/src/Pages/EditPage/EditProfile.js
+++ pard_3rd_leeyouhyeon_todayhome/src/Pages/EditPage/EditProfile.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "../../Header";
+
 
 function EditProfile() {
   return (
     <div>
+      <Header subIndex="설정"/>
     </div>
   );
 }
